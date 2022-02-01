82baccf2b2cf754f6baa06983b603789aac4dabb
Chaminda Chanaka <chanaka@itplace.com>
2022-02-01 12:20:09 +0530

    create searchbar component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <SearchBar></SearchBar>
+
+  </div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar.vue";
+export default {
+  name: 'App',
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+<h3>Search Box </h3>
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Search Box" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <span class="input-group-text btn btn-info" id="basic-addon2">Search</span>
+  </div>
+</div>
+</div>
+</template>
+<script>
+export default {
+  name: 'search-bar',
+  props: {
+  },
+  setup(){
+
+    return {}
+  }
+}
+</script>
